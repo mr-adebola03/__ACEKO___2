1e81af9f451edf53fe8ff474d1f73fc963259b49
Jordik-rel <ezechielcatraille@gmail.com>
2025-04-07 15:15:49 +0200

    add notifications header

--- FrontAceko2/aceko-project/src/constants/index.js
+++ FrontAceko2/aceko-project/src/constants/index.js
@@ -89,32 +89,7 @@ export const notifications = [
     badge: FaArrowUpRightFromSquare
   },
 ]
-// export const notifications = [
-//   {
-//     href: "/docteur/notification",
-//     icon: user01,
-//     text: "Resultats examen disponible",
-//     badge: FaArrowUpRightFromSquare
-//   },
-//   {
-//     href: "/docteur/notification",
-//     icon: user02,
-//     text: "Urgence patient",
-//     badge: FaArrowUpRightFromSquare
-//   },
-//   {
-//     href: "/docteur/notification",
-//     icon: user03,
-//     text: "Patient en crise",
-//     badge: FaArrowUpRightFromSquare
-//   },
-//   {
-//     href: "/docteur/notification",
-//     icon: user01,
-//     text: "Le patient n'a pas respecté la marge prevue pour son rendez-vous",
-//     badge: FaArrowUpRightFromSquare
-//   },
-// ]
+
 
 export const getEmployeesData = (counts) => [
   {
