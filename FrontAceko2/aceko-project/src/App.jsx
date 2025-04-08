6e54ecab452af0d6621b53c53ca191bd3f9e4f17
mr-adebola03 <159397310+mr-adebola03@users.noreply.github.com>
2025-04-08 19:27:38 +0100

    Update a configuré

--- FrontAceko2/aceko-project/src/App.jsx
+++ FrontAceko2/aceko-project/src/App.jsx
@@ -31,14 +31,14 @@ function App() {
           <Route path='/registration-success' element={<RegistrationSuccess/>} />
           <Route path='/forgot-password' element={<ForgotPassword/>}  />
           <Route path='/admin/*' element={
-              <PrivateRoute> 
+              // <PrivateRoute> 
                 <AdminDash/>
-              </PrivateRoute>
+              // </PrivateRoute>
           }/>
           <Route path='/docteur/*' element={
-            <PrivateRoute> 
+            // <PrivateRoute> 
               <DocteurDash/>
-            </PrivateRoute>
+            // </PrivateRoute>
             } />
           <Route path='/signup-step1' element={<SignupStep1 />} />
           <Route path='/signup-step2' element={<SignupStep2 />} />
