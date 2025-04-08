d5a252a5d268d364fe29a84cbfd2c750f0c8a360
mr-adebola03 <159397310+mr-adebola03@users.noreply.github.com>
2025-04-08 06:39:51 +0100

    Update urls.py

--- backaceko/auth_core/urls.py
+++ backaceko/auth_core/urls.py
@@ -7,9 +7,17 @@ from .views import (
     LoginView,
     ApproveUserView,
     ChangePasswordView,
+    RejectUserView,
+    RejectedUsersListView,
+    SpecialiteLaboratoireList,
+    SpecialiteMedicaleList,
+    UserDetailView,
     UserProfileView,
-    PendingApprovalListView,
+    PendingApprovalListView, OptionsView,
+    ApprovalListView
 )
+from src import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -18,8 +26,15 @@ urlpatterns = [
     path('profile/', UserProfileView.as_view(), name='profile'),
     path('change-password/', ChangePasswordView.as_view(), name='change-password'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('specialites-medicales/', SpecialiteMedicaleList.as_view(), name='specialites-medicales'),
+    path('specialites-laboratoires/', SpecialiteLaboratoireList.as_view(), name='specialites-laboratoires'),
+    path('option/',OptionsView.as_view(), name='options'),
     
     # admin endpoints
+    path('admin/users/<int:pk>/', UserDetailView.as_view(), name='admin-user-detail'),
     path('admin/users/<int:pk>/approve/', ApproveUserView.as_view(), name='approve-user'),
     path('admin/pending-approvals/', PendingApprovalListView.as_view(), name='pending-approvals'),
-]
+    path('admin/users-approvals/', ApprovalListView.as_view(), name='users-approvals'),
+    path('admin/users/<int:pk>/reject/', RejectUserView.as_view(), name='reject-user'),
+    path('admin/users/rejected/', RejectedUsersListView.as_view(), name='rejected-users'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
