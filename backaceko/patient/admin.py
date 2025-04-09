b7634516f67d83d50e41153e60610d23183e47e2
mr-adebola03 <159397310+mr-adebola03@users.noreply.github.com>
2025-04-09 01:09:31 +0100

    Affichage du model Patients sur la page admin

--- backaceko/patient/admin.py
+++ backaceko/patient/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from patient.models import CustomPatient
 
 # Register your models here.
+class PatientAdmin(admin.ModelAdmin):
+        list_display = ('numerodossier', 'first_name', 'last_name', 'civilite', 'stade_mrc','docteur')
+        
+admin.site.register(CustomPatient, PatientAdmin)
