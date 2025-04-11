c228b6b4e03308ea6963b99da55abcaf653a66cb
Jordik-rel <ezechielcatraille@gmail.com>
2025-04-11 17:08:57 +0200

    Laborantin2

---
+++ FrontAceko2/aceko-project/src/WorkFlow/workflowDashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const workflowDashboard = () => {
+  return (
+    <div>workflowDashboard</div>
+  )
+}
+
+export default workflowDashboard
--- FrontAceko2/aceko-project/src/routes/dash/DocteurDash/Patient/AddNewConsultation.jsx
+++ FrontAceko2/aceko-project/src/routes/dash/DocteurDash/Patient/AddNewConsultation.jsx
@@ -236,77 +236,6 @@ const AddNewConsultation = () => {
         }
     };
 
-<<<<<<< Updated upstream
-    return (
-        <Content>
-            <div>
-                <Link to="/docteur/patient-historique-consultation" className='flex justify-between items-center mb-4'>
-                    <IoIosArrowRoundBack className='text-3xl cursor-pointer' />
-                    <p className='w-[60%] text-xl text-slate-500 font-medium'>New Consultation</p>
-                </Link>
-                <div className='w-[90%] p-4 bg-slate-100 rounded-md shadow-md mx-auto mb-4'>
-                    <h2 className='font-bold text-gray-700 text-2xl text-center mb-4'>Add New Consultation</h2>
-                    <form onSubmit={handleSubmit} className='px-2'>
-                        {errors.general && (
-                            <div className="text-red-500 text-sm mb-2">{errors.general}</div>
-                        )}
-
-                        <div className="col">
-                            <Name name={formData.name} onNameChange={handleChange('name')} error={errors.name} />
-                        </div>
-                        <div className="col">
-                            <Motif motif={formData.motif} onMotifChange={handleChange('motif')} error={errors.motif} />
-                        </div>
-
-                        <PatientSelect
-                            patient={formData.patient}
-                            onPatientChange={handleChange('patient')}
-                            options={patientOptions}
-                            error={errors.patient}
-                        />
-
-                        <DateInput
-                            date={formData.date}
-                            onDateChange={handleChange('date')}
-                            error={errors.date}
-                        />
-
-                        <div className="mb-4 px-2">
-                            <label className="block text-sm font-medium text-gray-700 mb-2">
-                                Observations
-                            </label>
-                            <textarea
-                                className="w-full border rounded p-3 h-40 outline-none bg-slate-200"
-                                placeholder="Ex: Trouble de la personnalité"
-                                value={formData.observation}
-                                onChange={(e) => handleChange('observation')(e.target.value)}
-                                required
-                            />
-                        </div>
-                        <div className="mb-4 px-2">
-                            <label className="block text-sm font-medium text-gray-700 mb-2">
-                                Rapport Consultation
-                            </label>
-                            <textarea
-                                className="w-full border rounded p-3 h-40 outline-none bg-slate-200"
-                                placeholder="Ex: Patient atteint à stade 3"
-                                value={formData.rapport}
-                                onChange={(e) => handleChange('rapport')(e.target.value)}
-                                required
-                            />
-                        </div>
-                        <div className='flex justify-center'>
-                            <button className='text-xl text-center font-semibold text-slate-100 w-[50%] bg-blue-600 p-2 rounded-sm m-auto'>
-                                Enregistrer
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </Content>
-    );
-};
-=======
   return (
     <Content>
       <div>
@@ -364,7 +293,6 @@ const AddNewConsultation = () => {
     </Content>
   )
 }
->>>>>>> Stashed changes
 
 const Name = ({ name, onNameChange, error }) => (
     <div className='mb-3'>
