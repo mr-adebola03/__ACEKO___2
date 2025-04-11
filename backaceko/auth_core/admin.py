5b11ca482dbeb690170a7af6e281df2cdd64da96
mr-adebola03 <159397310+mr-adebola03@users.noreply.github.com>
2025-04-11 15:26:16 +0100

    update admin

--- backaceko/auth_core/admin.py
+++ backaceko/auth_core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CustomUser 
+from .models import CustomUser, SpecialiteLaboratoire, SpecialiteMedicale 
 from django.contrib.auth.admin import UserAdmin
 
 admin.site.site_header = "ACEKO"
@@ -10,4 +10,12 @@ class UserAdmin(UserAdmin):
         list_display = ('email', 'username', 'agents_sante', 'is_approved')
         list_editable = ('is_approved',)
 
+class SpecialiteMedicaleAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'description')
+    
+class SpecialiteLaboratoireAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'description')
+
 admin.site.register(CustomUser, UserAdmin)
+admin.site.register(SpecialiteMedicale, SpecialiteMedicaleAdmin)
+admin.site.register(SpecialiteLaboratoire, SpecialiteLaboratoireAdmin)
